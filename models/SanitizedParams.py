cd87845cc6d54ae1289cb501593a0e8d4677681e
Luke Swanson <lwswanson2@wisc.edu>
2024-04-03 09:40:45 -0500

    Move the SanitizedParams class to own file.

---
+++ models/SanitizedParams.py
@@ -0,0 +1,57 @@
+# standard imports
+import datetime, re
+from typing import Optional
+
+# 3rd-party imports
+from flask import request
+
+# local imports
+
+class SanitizedParams:
+    """Dumb struct to store the sanitized params from a request
+    """
+    def __init__(self, game_id:Optional[str], year:int, month:int):
+        self._game_id : Optional[str] = game_id
+        self._year    : int           = year
+        self._month   : int           = month
+    
+    @property
+    def GameID(self) -> Optional[str]:
+        return self._game_id
+    @property
+    def Year(self) -> int:
+        return self._year
+    @property
+    def Month(self) -> int:
+        return self._month
+
+    # If the given game_id contains allowed characters, return it in UPPERCASE, otherwise return empty string
+    @staticmethod
+    def sanitizeGameId(game_id: str) -> str:
+        if re.search("^[A-Za-z_]+$", game_id) is None:
+            game_id = ""
+        return game_id.upper()
+
+    # Shared utility function to retrieve game_id, year, and month from the request's query string.
+    # Defaults are used if a value was not given or is invalid
+    @staticmethod
+    def FromRequest() -> 'SanitizedParams':
+
+        now_date = datetime.date.today()
+
+        # Extract query string parameters
+        game_id = SanitizedParams.sanitizeGameId(request.args.get("game_id", default ="", type=str))
+        year = request.args.get("year",   default=now_date.year, type=int)
+        month = request.args.get("month", default=now_date.month, type=int)
+
+        if game_id == "":
+            game_id = None
+        
+        if month < 1 or month > 12:
+            month = now_date.month
+
+        if year < 2000 or year > now_date.year:
+            year = now_date.year
+
+        return SanitizedParams(game_id=game_id, year=year, month=month)
+        return { "game_id": game_id, "year": year, "month": month }
