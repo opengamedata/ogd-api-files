06811c7129d77d2097ed0a9b90b13dbb367e41c1
Luke Swanson <lwswanson2@wisc.edu>
2024-05-30 16:59:31 -0500

    Ok, go one more folder deep, with actual unit tests in 'cases' subfolder.

--- TestDriver.py
+++ TestDriver.py
@@ -1,6 +1,6 @@
-from tests.HelloAPI.t_HelloAPI import t_HelloAPI
 from tests.config.t_config import settings
 from tests.schemas.TestConfigSchema import TestConfigSchema
+from tests.cases.HelloAPI.t_HelloAPI import t_HelloAPI
 
 _cfg = TestConfigSchema.FromDict(name="TestDriverConfig", all_elements=settings, logger=None)
 if _cfg.EnabledTests.get('HELLO', False):
