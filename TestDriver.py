aef30f155a2969cc53f444d16401e2dc737e0343
Luke Swanson <lwswanson2@wisc.edu>
2024-05-30 14:09:31 -0500

    Slightly nicer check for enabled test.

--- TestDriver.py
+++ TestDriver.py
@@ -2,7 +2,7 @@ from tests.t_HelloAPI import t_HelloAPI
 from tests.t_config import EnabledTests
 
 
-if EnabledTests.get('HELLO'):
+if EnabledTests.get('HELLO', False):
     test_Hello = t_HelloAPI()
     print("***\nRunning test_Hello:")
     test_Hello.RunAll()
