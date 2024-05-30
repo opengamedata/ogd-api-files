f7273c712370d282290fa0cf75f7cfd9e13a3c6e
Luke Swanson <lwswanson2@wisc.edu>
2024-05-30 16:56:48 -0500

    Ok, also need to update TestDriver to work with config schema.

--- TestDriver.py
+++ TestDriver.py
@@ -1,8 +1,9 @@
 from tests.HelloAPI.t_HelloAPI import t_HelloAPI
-from tests.config.t_config import EnabledTests
+from tests.config.t_config import settings
+from tests.schemas.TestConfigSchema import TestConfigSchema
 
-
-if EnabledTests.get('HELLO', False):
+_cfg = TestConfigSchema.FromDict(name="TestDriverConfig", all_elements=settings, logger=None)
+if _cfg.EnabledTests.get('HELLO', False):
     test_Hello = t_HelloAPI()
     print("***\nRunning test_Hello:")
     test_Hello.RunAll()
