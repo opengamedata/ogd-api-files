aa60bc0a9062fef52a35f8a1aae029fc0ce9b0c8
Luke Swanson <lwswanson2@wisc.edu>
2025-10-21 00:18:24 -0500

    Create a basic config class for File API.
    Doesn't do any real parsing, should do that in the future.
    For now, we naively assume the input dict has the right stuff with right
    types.

---
+++ src/schemas/FileAPIConfig.py
@@ -0,0 +1,44 @@
+"""
+ServerConfigSchema
+
+Contains a Schema class for managing config data for server configurations.
+"""
+
+# import standard libraries
+import logging
+from typing import Any, Dict, Final
+
+# import 3rd-party libraries
+
+# import OGD libraries
+from ogd.common.utils.SemanticVersion import SemanticVersion
+from ogd.apis.schemas.ServerConfigSchema import ServerConfigSchema
+
+# import local files
+
+class FileAPIConfig(ServerConfigSchema):
+    _DEFAULT_FILE_LIST_URL : Final[str] = 'https://opengamedata.fielddaylab.wisc.edu/data/file_list.json'
+
+
+    def __init__(self, name:str, all_elements:Dict[str, Any]):
+        self._game_mapping  : Dict[str, Dict[str, str]] = all_elements.get("BIGQUERY_GAME_MAPPING", {})
+        self._file_list_url : str                       = all_elements.get("FILE_LIST_URL", FileAPIConfig._DEFAULT_FILE_LIST_URL)
+
+        _used = {"DB_CONFIG", "OGD_CORE_PATH", "GOOGLE_CLIENT_ID"}
+        _leftovers = { key : val for key,val in all_elements.items() if key not in _used }
+        super().__init__(name=name, debug_level=logging.INFO, version=SemanticVersion(0,0,0,"version-not-set"), other_elements=_leftovers)
+
+    @property
+    def GameMapping(self) -> Dict[str, Dict[str, str]]:
+        return self._game_mapping
+
+    @property
+    def FileListURL(self) -> str:
+        return self._file_list_url
+
+    @property
+    def AsMarkdown(self) -> str:
+        ret_val : str
+
+        ret_val = f"{self.Name}"
+        return ret_val
