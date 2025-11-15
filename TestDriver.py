06021e93ecc799d78244253fddd89aada00042cb
Luke Swanson <lwswanson2@wisc.edu>
2025-11-14 21:04:13 -0600

    Remove old TestDriver, we don't use it.

--- TestDriver.py
+++
@@ -1,9 +0,0 @@
-from tests.config.t_config import settings
-from tests.config.FileAPITestConfig import FileAPITestConfig
-from tests.cases.t_HelloAPI import t_HelloAPI
-
-_cfg = FileAPITestConfig.FromDict(name="FileAPITestConfig", all_elements=settings, logger=None)
-if _cfg.EnabledTests.get('HELLO', False):
-    print("***\nRunning t_HelloAPI:")
-    t_HelloAPI.RunAll()
-    print("Done\n***")
