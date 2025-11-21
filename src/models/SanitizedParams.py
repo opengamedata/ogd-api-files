6e0354db04357a671195f40698e56550adbc33b3
Luke Swanson <lwswanson2@wisc.edu>
2025-11-21 17:01:35 -0600

    Do a bit more to sanitize year and month in our SanitizedParams class.

--- src/models/SanitizedParams.py
+++ src/models/SanitizedParams.py
@@ -1,6 +1,6 @@
 # standard imports
 import datetime, re
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, Union
 
 # 3rd-party imports
 from flask_restful import reqparse
@@ -27,10 +27,38 @@ class SanitizedParams:
 
     # If the given game_id contains allowed characters, return it in UPPERCASE, otherwise return empty string
     @staticmethod
-    def sanitizeGameId(game_id: str) -> str:
+    def sanitizeGameId(game_id:str) -> str:
         if re.search("^[A-Za-z_]+$", game_id) is None:
             game_id = ""
         return game_id.upper()
+
+    @staticmethod
+    def sanitizeYear(year:Union[int, str], default_date:datetime.date=datetime.date.today()) -> int:
+        ret_val: int
+
+        if isinstance(year, int):
+            ret_val = year
+        else:
+            if re.search("^[1-9]+$", str(year)) is None:
+                ret_val = default_date.year
+            else:
+                ret_val = int(str(year))
+
+        return ret_val
+
+    @staticmethod
+    def sanitizeMonth(month:Union[int, str], default_date:datetime.date=datetime.date.today()) -> int:
+        ret_val: int
+
+        if isinstance(month, int):
+            ret_val = month
+        else:
+            if re.search("^[1-9]+$", str(month)) is None:
+                ret_val = default_date.month
+            else:
+                ret_val = int(str(month))
+
+        return ret_val
 
     # Shared utility function to retrieve game_id, year, and month from the request's query string.
     # Defaults are used if a value was not given or is invalid
@@ -44,9 +72,9 @@ class SanitizedParams:
         parser.add_argument("month",   type=int, nullable=True, required=False, default=default_date.month, location="args")
         args : Dict[str, Any] = parser.parse_args()
 
-        game_id = SanitizedParams.sanitizeGameId(args.get("game_id", ""))
-        year    = args.get("year")  or default_date.year
-        month   = args.get("month") or default_date.month
+        game_id : Optional[str] = SanitizedParams.sanitizeGameId(args.get("game_id", ""))
+        year    : int           = SanitizedParams.sanitizeYear(args.get("year",  default_date.year))
+        month   : int           = SanitizedParams.sanitizeMonth(args.get("month", default_date.month))
 
         if game_id == "":
             game_id = None
