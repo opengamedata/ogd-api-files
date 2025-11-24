1437a54a62aed7f3cfec130ddc93134746a60f55
Luke Swanson <lwswanson2@wisc.edu>
2025-11-24 14:28:30 -0600

    Reintroduce hack for conig to GameList, forgot we still need that so parsing doesn't crash.

--- src/apis/FileAPI.py
+++ src/apis/FileAPI.py
@@ -63,6 +63,12 @@ class FileAPI:
             # Pull the file list data into a dictionary
             file_list_response                             = url_request.urlopen(FileAPI.server_config.FileListURL)
             file_list_json     : Dict[str, Dict[str, Any]] = json.loads(file_list_response.read())
+            # HACK to make sure we've got a remote_url, working around bug in RepositoryIndexingConfig FromDict(...) implementation.
+            if "CONFIG" in file_list_json.keys() and isinstance(file_list_json["CONFIG"], dict):
+                if not "remote_url" in file_list_json["CONFIG"].keys():
+                    file_list_json["CONFIG"]["remote_url"] = file_list_json["CONFIG"].get("files_base", "https://opengamedata.fielddaylab.wisc.edu/")
+                if not "templates_url" in file_list_json["CONFIG"].keys():
+                    file_list_json["CONFIG"]["templates_url"] = file_list_json["CONFIG"].get("templates_base", "https://github.com/opengamedata/opengamedata-templates")
             file_list          : DatasetRepositoryConfig   = DatasetRepositoryConfig.FromDict(name="file_list", unparsed_elements=file_list_json)
 
             # If the given game isn't in our dictionary, or our dictionary doesn't have any date ranges for this game
