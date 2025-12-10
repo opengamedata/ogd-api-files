97cbebe6e7c4672e68aab58c33102ee29af68e16
Luke Swanson <lwswanson2@wisc.edu>
2025-12-10 08:17:06 -0600

    Try making the API config a singleton, which theoretically will allow us to access from resources that aren't part of FileAPI class.

--- src/configs/FileAPIConfig.py
+++ src/configs/FileAPIConfig.py
@@ -5,31 +5,38 @@ Contains a Schema class for managing config data for server configurations.
 """
 
 # import standard libraries
-from typing import Any, Dict, Final
+from typing import Any, Dict, Final, Optional, Self
 
 # import 3rd-party libraries
 
 # import OGD libraries
 from ogd.apis.configs.ServerConfig import ServerConfig
+from ogd.common.utils.typing import Map
 
 # import local files
 
 class FileAPIConfig(ServerConfig):
     _DEFAULT_FILE_LIST_URL : Final[str] = 'https://opengamedata.fielddaylab.wisc.edu/data/file_list.json'
 
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, '_instance'):
+            cls._instance = super(FileAPIConfig, cls).__new__(cls)
+        return cls._instance
 
     def __init__(self, name:str, all_elements:Dict[str, Any]):
-        self._game_mapping  : Dict[str, Dict[str, str]] = all_elements.get("BIGQUERY_GAME_MAPPING", {})
-        self._file_list_url : str                       = all_elements.get("FILE_LIST_URL", FileAPIConfig._DEFAULT_FILE_LIST_URL)
+        if not hasattr(self, '_initialized'):
+            self._game_mapping  : Dict[str, Dict[str, str]] = all_elements.get("BIGQUERY_GAME_MAPPING", {})
+            self._file_list_url : str                       = all_elements.get("FILE_LIST_URL", FileAPIConfig._DEFAULT_FILE_LIST_URL)
 
-        _used = {"DB_CONFIG", "OGD_CORE_PATH", "GOOGLE_CLIENT_ID"}
-        _leftovers = { key : val for key,val in all_elements.items() if key not in _used }
-        super().__init__(
-            name=name,
-            debug_level=None,
-            version=None,
-            other_elements=_leftovers
-        )
+            _used = {"DB_CONFIG", "OGD_CORE_PATH", "GOOGLE_CLIENT_ID"}
+            _leftovers = { key : val for key,val in all_elements.items() if key not in _used }
+            super().__init__(
+                name=name,
+                debug_level=None,
+                version=None,
+                other_elements=_leftovers
+            )
+            self._initialized = True
 
     @property
     def GameMapping(self) -> Dict[str, Dict[str, str]]:
@@ -46,6 +53,6 @@ class FileAPIConfig(ServerConfig):
         ret_val = f"{self.Name}"
         return ret_val
 
-    @staticmethod
-    def FromDict(name:str, all_elements:Dict[str, Any]) -> "FileAPIConfig":
-        return FileAPIConfig(name=name, all_elements=all_elements)
+    @classmethod
+    def _fromDict(cls, name:str, unparsed_elements:Map, key_overrides:Optional[Dict[str, str]]=None, default_override:Optional[Self]=None) -> "FileAPIConfig":
+        return FileAPIConfig(name=name, all_elements=unparsed_elements)
