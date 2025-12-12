94f72ab321535b1e8aa05b151372b572fb3b692b
Luke Swanson <lwswanson2@wisc.edu>
2025-12-12 13:48:01 -0600

    Update the endpoints that get specific datasets to yield a 404 if dataset is not found.

--- src/apis/FileAPI.py
+++ src/apis/FileAPI.py
@@ -178,68 +178,73 @@ class FileAPI:
         def get(self, game_id, month, year):
             ret_val = APIResponse.Default(req_type=RESTType.GET)
 
-            last_month = date.today().replace(day=1) - timedelta(days=1)
-            sanitized_request = SanitizedParams(game_id=game_id, year=year, month=month, default_date=last_month)
+            try:
+                last_month = date.today().replace(day=1) - timedelta(days=1)
+                sanitized_request = SanitizedParams(game_id=game_id, year=year, month=month, default_date=last_month)
 
-        # 1. Get the list of datasets available on the server, for given game.
-            file_list     : DatasetRepositoryConfig = FileAPI._getFileList(FileAPI.server_config.FileListURL)
-            game_datasets : DatasetCollectionSchema = file_list.Games.get(sanitized_request.GameID or "NO GAME REQUESTED", DatasetCollectionSchema.Default())
+            # 1. Get the list of datasets available on the server, for given game.
+                file_list     : DatasetRepositoryConfig = FileAPI._getFileList(FileAPI.server_config.FileListURL)
+                game_datasets : DatasetCollectionSchema = file_list.Games.get(sanitized_request.GameID or "NO GAME REQUESTED", DatasetCollectionSchema.Default())
 
-            # If we couldn't find the requested game in file_list.json, or the game didn't have any date ranges, skip.
-            if (sanitized_request.GameID is None):
-                ret_val.RequestErrored(msg=f"Bad GameID '{sanitized_request.GameID}'")
-                return ret_val.AsFlaskResponse
-            elif (len(game_datasets.Datasets) == 0):
-                ret_val.ServerErrored(msg=f"GameID '{sanitized_request.GameID}' did not have available datasets")
-                return ret_val.AsFlaskResponse
-            # Else, continue on.
-
-        # 2. Search for the most recently modified dataset that contains the requested month and year
-            _matched_dataset : Optional[DatasetSchema] = FileAPI._matchDataset(sanitized_request=sanitized_request, game_datasets=game_datasets)
-
-            if _matched_dataset and _matched_dataset.Key.DateFrom and _matched_dataset.Key.DateTo:
-                file_info = {}
-
-                # If this range contains the given year & month
-                # Base URLs
-                CODESPACES_BASE_URL : str = "https://codespaces.new/opengamedata/opengamedata-samples/tree/"
-                GITHUB_BASE_URL     : str = "https://github.com/opengamedata/opengamedata-core/tree/"
-                
-                # Date information
-                file_info["first_year"]  = _matched_dataset.Key.DateFrom.year
-                file_info["first_month"] = _matched_dataset.Key.DateFrom.month
-                file_info["last_year"]   = _matched_dataset.Key.DateTo.year
-                file_info["last_month"]  = _matched_dataset.Key.DateTo.month
-                _branch_name     = sanitized_request.GameID.lower().replace('_', '-')
-                _revision        = _matched_dataset.OGDRevision or None
-
-                # Files
-                file_info["raw_file"]        = f"{file_list.RemoteURL}{_matched_dataset.GameEventsFile}" if _matched_dataset.GameEventsFile is not None else None
-                file_info["events_file"]     = f"{file_list.RemoteURL}{_matched_dataset.AllEventsFile}"  if _matched_dataset.AllEventsFile  is not None else None
-                file_info["sessions_file"]   = f"{file_list.RemoteURL}{_matched_dataset.SessionsFile}"   if _matched_dataset.SessionsFile   is not None else None
-                file_info["players_file"]    = f"{file_list.RemoteURL}{_matched_dataset.PlayersFile}"    if _matched_dataset.PlayersFile    is not None else None
-                file_info["population_file"] = f"{file_list.RemoteURL}{_matched_dataset.PopulationFile}" if _matched_dataset.PopulationFile is not None else None
-
-                # Templates
-                file_info["events_template"]     = f"{file_list.TemplatesBase}{_matched_dataset.EventsTemplate}"     if _matched_dataset.EventsTemplate     is not None else None
-                file_info["sessions_template"]   = f"{file_list.TemplatesBase}{_matched_dataset.SessionsTemplate}"   if _matched_dataset.SessionsTemplate   is not None else None
-                file_info["players_template"]    = f"{file_list.TemplatesBase}{_matched_dataset.PlayersTemplate}"    if _matched_dataset.PlayersTemplate    is not None else None
-                file_info["population_template"] = f"{file_list.TemplatesBase}{_matched_dataset.PopulationTemplate}" if _matched_dataset.PopulationTemplate is not None else None
-
-                file_info["events_codespace"]   = f"{CODESPACES_BASE_URL}{_branch_name}?quickstart=1&devcontainer_path=.devcontainer%2Fevent-template%2Fdevcontainer.json"
-                file_info["sessions_codespace"] = f"{CODESPACES_BASE_URL}{_branch_name}?quickstart=1&devcontainer_path=.devcontainer%2Fplayer-template%2Fdevcontainer.json"
-                file_info["players_codespace"]  = f"{CODESPACES_BASE_URL}{_branch_name}?quickstart=1&devcontainer_path=.devcontainer%2Fsession-template%2Fdevcontainer.json"
-
-                # Convention for branch naming is lower-case with dashes,
-                # while game IDs are usually upper-case with underscores, so make sure we do the conversion
-                file_info["detectors_link"] = f"{GITHUB_BASE_URL}{_revision}/src/ogd/games/{sanitized_request.GameID.upper()}/detectors" if _revision else None
-                file_info["features_link"]  = f"{GITHUB_BASE_URL}{_revision}/src/ogd/games/{sanitized_request.GameID.upper()}/features"  if _revision else None
-                file_info["found_matching_range"] = True
-
-                ret_val.RequestSucceeded(msg="Retrieved game file info by month", val=file_info)
+                # If we couldn't find the requested game in file_list.json, or the game didn't have any date ranges, skip.
+                if (sanitized_request.GameID is None):
+                    ret_val.RequestErrored(msg=f"Bad GameID '{sanitized_request.GameID}'")
+                    return ret_val.AsFlaskResponse
+                elif (len(game_datasets.Datasets) == 0):
+                    ret_val.ServerErrored(msg=f"GameID '{sanitized_request.GameID}' did not have available datasets")
+                    return ret_val.AsFlaskResponse
+            except Exception:
+                ret_val.ServerErrored("Unexpected error processing request inputs.")
             else:
-                _msg = f"Dataset key {_matched_dataset.Key} was invalid." if _matched_dataset else "No datasets found!"
-                ret_val.RequestErrored(msg=_msg)
+            # 2. Search for the most recently modified dataset that contains the requested month and year
+                _matched_dataset : Optional[DatasetSchema] = FileAPI._matchDataset(sanitized_request=sanitized_request, game_datasets=game_datasets)
+
+                if _matched_dataset:
+                    if _matched_dataset.Key.DateFrom and _matched_dataset.Key.DateTo:
+                        file_info = {}
+
+                        # If this range contains the given year & month
+                        # Base URLs
+                        CODESPACES_BASE_URL : str = "https://codespaces.new/opengamedata/opengamedata-samples/tree/"
+                        GITHUB_BASE_URL     : str = "https://github.com/opengamedata/opengamedata-core/tree/"
+                        
+                        # Date information
+                        file_info["first_year"]  = _matched_dataset.Key.DateFrom.year
+                        file_info["first_month"] = _matched_dataset.Key.DateFrom.month
+                        file_info["last_year"]   = _matched_dataset.Key.DateTo.year
+                        file_info["last_month"]  = _matched_dataset.Key.DateTo.month
+                        _branch_name     = sanitized_request.GameID.lower().replace('_', '-')
+                        _revision        = _matched_dataset.OGDRevision or None
+
+                        # Files
+                        file_info["raw_file"]        = f"{file_list.RemoteURL}{_matched_dataset.GameEventsFile}" if _matched_dataset.GameEventsFile is not None else None
+                        file_info["events_file"]     = f"{file_list.RemoteURL}{_matched_dataset.AllEventsFile}"  if _matched_dataset.AllEventsFile  is not None else None
+                        file_info["sessions_file"]   = f"{file_list.RemoteURL}{_matched_dataset.SessionsFile}"   if _matched_dataset.SessionsFile   is not None else None
+                        file_info["players_file"]    = f"{file_list.RemoteURL}{_matched_dataset.PlayersFile}"    if _matched_dataset.PlayersFile    is not None else None
+                        file_info["population_file"] = f"{file_list.RemoteURL}{_matched_dataset.PopulationFile}" if _matched_dataset.PopulationFile is not None else None
+
+                        # Templates
+                        file_info["events_template"]     = f"{file_list.TemplatesBase}{_matched_dataset.EventsTemplate}"     if _matched_dataset.EventsTemplate     is not None else None
+                        file_info["sessions_template"]   = f"{file_list.TemplatesBase}{_matched_dataset.SessionsTemplate}"   if _matched_dataset.SessionsTemplate   is not None else None
+                        file_info["players_template"]    = f"{file_list.TemplatesBase}{_matched_dataset.PlayersTemplate}"    if _matched_dataset.PlayersTemplate    is not None else None
+                        file_info["population_template"] = f"{file_list.TemplatesBase}{_matched_dataset.PopulationTemplate}" if _matched_dataset.PopulationTemplate is not None else None
+
+                        file_info["events_codespace"]   = f"{CODESPACES_BASE_URL}{_branch_name}?quickstart=1&devcontainer_path=.devcontainer%2Fevent-template%2Fdevcontainer.json"
+                        file_info["sessions_codespace"] = f"{CODESPACES_BASE_URL}{_branch_name}?quickstart=1&devcontainer_path=.devcontainer%2Fplayer-template%2Fdevcontainer.json"
+                        file_info["players_codespace"]  = f"{CODESPACES_BASE_URL}{_branch_name}?quickstart=1&devcontainer_path=.devcontainer%2Fsession-template%2Fdevcontainer.json"
+
+                        # Convention for branch naming is lower-case with dashes,
+                        # while game IDs are usually upper-case with underscores, so make sure we do the conversion
+                        file_info["detectors_link"] = f"{GITHUB_BASE_URL}{_revision}/src/ogd/games/{sanitized_request.GameID.upper()}/detectors" if _revision else None
+                        file_info["features_link"]  = f"{GITHUB_BASE_URL}{_revision}/src/ogd/games/{sanitized_request.GameID.upper()}/features"  if _revision else None
+                        file_info["found_matching_range"] = True
+
+                        ret_val.RequestSucceeded(msg="Retrieved game file info by month", val=file_info)
+                    else:
+                        _msg = f"Dataset key {_matched_dataset.Key} was invalid." if _matched_dataset else "No datasets found!"
+                        ret_val.RequestErrored(msg=_msg)
+                else:
+                    ret_val.RequestErrored(msg=f"Could not find a dataset for {game_id} in {month:02}/{year:04}", status=ResponseStatus.ERR_NOTFOUND)
 
             return ret_val.AsFlaskResponse
 
@@ -272,48 +277,52 @@ class FileAPI:
                 elif (len(game_datasets.Datasets) == 0):
                     ret_val.ServerErrored(msg=f"GameID '{sanitized_request.GameID}' did not have available datasets")
                     return ret_val.AsFlaskResponse
-                # Else, continue on.
-
+            except Exception:
+                ret_val.ServerErrored("Unexpected error processing request inputs.")
+            else:
             # 2. Search for the most recently modified dataset that contains the requested month and year
-                _matched_dataset : Optional[DatasetSchema] = FileAPI._matchDataset(sanitized_request=sanitized_request, game_datasets=game_datasets)
+                try:
+                    _matched_dataset : Optional[DatasetSchema] = FileAPI._matchDataset(sanitized_request=sanitized_request, game_datasets=game_datasets)
 
-                if _matched_dataset and _matched_dataset.Key.DateFrom and _matched_dataset.Key.DateTo:
-                    file_link = None
-                    missing_file_msg = f"Dataset for {game_id} from {f'{month:02}/{year:04}'} was not found."
-                    match str(file_type).upper():
-                        case "SESSION":
-                            file_link = f"{file_list.RemoteURL}{_matched_dataset.SessionsFile.as_posix()}"   if _matched_dataset.SessionsFile   is not None else None
-                        case "PLAYER":
-                            file_link = f"{file_list.RemoteURL}{_matched_dataset.PlayersFile.as_posix()}"    if _matched_dataset.PlayersFile    is not None else None
-                        case "POPULATION":
-                            file_link = f"{file_list.RemoteURL}{_matched_dataset.PopulationFile.as_posix()}" if _matched_dataset.PopulationFile is not None else None
-                        case "EVENT":
-                            missing_file_msg="Event files are not yet supported."
-                        case _:
-                            missing_file_msg=f"Unrecognized file type {file_type}."
-                    if file_link:
-                        datafile_response = url_request.urlopen(file_link)
-                        with zipfile.ZipFile(BytesIO(datafile_response.read())) as zipped:
-                            for f_name in zipped.namelist():
-                                if f_name.endswith(".tsv"):
-                                    data = pd.read_csv(zipped.open(f_name), sep="\t").replace({float('nan'):None})
-                                    data = self._secondaryParse(data)
-                                    result = {
-                                        "columns": list(data.columns),
-                                        "rows": list(data.apply(lambda series : series.to_dict(), axis=1))
-                                    }
-                                    ret_val.RequestSucceeded(msg="Retrieved game file info by month", val=result)
+                    if _matched_dataset:
+                        if _matched_dataset.Key.DateFrom and _matched_dataset.Key.DateTo:
+                            file_link = None
+                            missing_file_msg = f"Dataset for {game_id} from {f'{month:02}/{year:04}'} was not found."
+                            match str(file_type).upper():
+                                case "SESSION":
+                                    file_link = f"{file_list.RemoteURL}{_matched_dataset.SessionsFile.as_posix()}"   if _matched_dataset.SessionsFile   is not None else None
+                                case "PLAYER":
+                                    file_link = f"{file_list.RemoteURL}{_matched_dataset.PlayersFile.as_posix()}"    if _matched_dataset.PlayersFile    is not None else None
+                                case "POPULATION":
+                                    file_link = f"{file_list.RemoteURL}{_matched_dataset.PopulationFile.as_posix()}" if _matched_dataset.PopulationFile is not None else None
+                                case "EVENT":
+                                    missing_file_msg="Event files are not yet supported."
+                                case _:
+                                    missing_file_msg=f"Unrecognized file type {file_type}."
+                            if file_link:
+                                datafile_response = url_request.urlopen(file_link)
+                                with zipfile.ZipFile(BytesIO(datafile_response.read())) as zipped:
+                                    for f_name in zipped.namelist():
+                                        if f_name.endswith(".tsv"):
+                                            data = pd.read_csv(zipped.open(f_name), sep="\t").replace({float('nan'):None})
+                                            data = self._secondaryParse(data)
+                                            result = {
+                                                "columns": list(data.columns),
+                                                "rows": list(data.apply(lambda series : series.to_dict(), axis=1))
+                                            }
+                                            ret_val.RequestSucceeded(msg="Retrieved game file info by month", val=result)
+                            else:
+                                ret_val.RequestErrored(msg=missing_file_msg)
+                        else:
+                            ret_val.RequestErrored(msg=f"Dataset key {_matched_dataset.Key} was invalid.")
                     else:
-                        ret_val.RequestErrored(msg=missing_file_msg)
-                else:
-                    _msg = f"Dataset key {_matched_dataset.Key} was invalid." if _matched_dataset else "No datasets found!"
-                    ret_val.RequestErrored(msg=_msg)
-            except url_error.HTTPError as err:
-                current_app.logger.error(f"HTTP error getting {file_type} file from {file_link}:\n{err}")
-                ret_val.ServerErrored(msg=f"Server experienced an error retrieving {file_type} file from {f'{month:02}/{year:04}'} for {game_id}.")
-            except Exception as err:
-                current_app.logger.error(f"Uncaught {type(err)} getting {file_type} file from {file_link}:\n{err}\n{err.__traceback__}")
-                ret_val.ServerErrored(msg=f"Server experienced an error retrieving {file_type} file from {f'{month:02}/{year:04}'} for {game_id}.")
+                        ret_val.RequestErrored(msg=f"Could not find a dataset for {game_id} in {month:02}/{year:04}", status=ResponseStatus.ERR_NOTFOUND)
+                except url_error.HTTPError as err:
+                    current_app.logger.error(f"HTTP error getting {file_type} file from {file_link}:\n{err}")
+                    ret_val.ServerErrored(msg=f"Server experienced an error retrieving {file_type} file from {f'{month:02}/{year:04}'} for {game_id}.")
+                except Exception as err:
+                    current_app.logger.error(f"Uncaught {type(err)} getting {file_type} file from {file_link}:\n{err}\n{err.__traceback__}")
+                    ret_val.ServerErrored(msg=f"Server experienced an error retrieving {file_type} file from {f'{month:02}/{year:04}'} for {game_id}.")
 
             return ret_val.AsFlaskResponse
 
