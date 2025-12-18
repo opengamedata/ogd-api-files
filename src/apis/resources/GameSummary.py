2a684e764ed5a0cab1c87ec948cdddc4a55894a9
Luke Swanson <lwswanson2@wisc.edu>
2025-12-18 10:53:40 -0600

    Initial implementation of the GameSummary endpoint.
    Will need some testing to ensure initial_dataset calculation runs
    correctly.

--- src/apis/resources/GameSummary.py
+++ src/apis/resources/GameSummary.py
@@ -5,9 +5,11 @@ from flask_restful import Resource
 # import ogd libraries
 from ogd.apis.utils.APIResponse import APIResponse, RESTType, ResponseStatus
 from ogd.common.configs.storage.DatasetRepositoryConfig import DatasetRepositoryConfig
+from ogd.common.schemas.datasets.DatasetCollectionSchema import DatasetCollectionSchema
 
 # import local files
 from apis.configs.FileAPIConfig import FileAPIConfig
+from models.SanitizedParams import SanitizedParams
 from utils.utils import GetFileList
 
 
@@ -22,7 +24,22 @@ class GameSummary(Resource):
     """
     def get(self, game_id):
         ret_val = APIResponse.Default(req_type=RESTType.GET)
+        
+        game_id = SanitizedParams.sanitizeGameId(game_id)
+        if game_id is None or game_id == "":
+            ret_val.RequestErrored(msg=f"Bad GameID '{game_id}'")
+            return ret_val.AsFlaskResponse
 
-        ret_val.ServerErrored("This endpoint is not yet implemented!")
+        cfg           : FileAPIConfig           = FileAPIConfig("FileAPIConfig", {})
+        file_list     : DatasetRepositoryConfig = GetFileList(cfg.FileListURL)
+        game_datasets : DatasetCollectionSchema = file_list.Games.get(game_id, DatasetCollectionSchema.Default())
+
+        # If the given game isn't in our dictionary, or our dictionary doesn't have any date ranges for this game
+        if not game_id in file_list.Games or len(file_list.Games[game_id].Datasets) == 0:
+            ret_val.ServerErrored(msg=f"GameID '{game_id}' not found in list of games with datasets, or had no datasets listed")
+            return ret_val.AsFlaskResponse
+
+        responseData = { "game_id": game_id, "dataset_count": len(game_datasets.Datasets), "initial_dataset": min(game_datasets.Datasets.values(), key=lambda dataset : dataset.StartDate) }
+        ret_val.RequestSucceeded(msg="Retrieved monthly game usage", val=responseData)
 
         return ret_val.AsFlaskResponse
