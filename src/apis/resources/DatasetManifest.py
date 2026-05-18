dec7100b6c6927d3b7801bf1f902a507bd0ab9e3
Luke Swanson <lwswanson2@wisc.edu>
2026-05-18 15:03:06 -0500

    Clean up some excess lines.

--- src/apis/resources/DatasetManifest.py
+++ src/apis/resources/DatasetManifest.py
@@ -65,35 +65,10 @@ class DatasetManifest(Resource):
                     # Base URLs
                     GITHUB_BASE_URL     : str = "https://github.com/opengamedata/opengamedata-core/tree/"
                     
-
-                    # File outputs
-                    file_info["output"] = {}
-                    file_info["output"]["raw_file"]        = f"{file_list.RemoteURL}{_matched_dataset.GameEventsFile}" if _matched_dataset.GameEventsFile is not None else None
-                    file_info["output"]["events_file"]     = f"{file_list.RemoteURL}{_matched_dataset.AllEventsFile}"  if _matched_dataset.AllEventsFile  is not None else None
-                    file_info["output"]["sessions_file"]   = f"{file_list.RemoteURL}{_matched_dataset.SessionsFile}"   if _matched_dataset.SessionsFile   is not None else None
-                    file_info["output"]["players_file"]    = f"{file_list.RemoteURL}{_matched_dataset.PlayersFile}"    if _matched_dataset.PlayersFile    is not None else None
-                    file_info["output"]["population_file"] = f"{file_list.RemoteURL}{_matched_dataset.PopulationFile}" if _matched_dataset.PopulationFile is not None else None
-
-                    # Versioning/traceability
-                    file_info["versioning"] = {}
                     # Convention for branch naming is lower-case with dashes,
                     # while game IDs are usually upper-case with underscores, so make sure we do the conversion
-                    file_info["versioning"]["detectors_link"] = f"{GITHUB_BASE_URL}{_matched_dataset.OGDRevision}/src/ogd/games/{sanitary_params.GameID.upper()}/detectors" if _matched_dataset.OGDRevision else None
-                    file_info["versioning"]["features_link"]  = f"{GITHUB_BASE_URL}{_matched_dataset.OGDRevision}/src/ogd/games/{sanitary_params.GameID.upper()}/features"  if _matched_dataset.OGDRevision else None
-
-                    raw_url = f"https://raw.githubusercontent.com/opengamedata/ogd-core/refs/heads/main/src/ogd/games/{sanitary_params.GameID.upper()}/schemas/{sanitary_params.GameID.upper()}.json.template"
-                    with urlrequest.urlopen(raw_url) as _conn:
-                        try:
-                            raw_schema = _conn.read().decode('utf-8')
-                        except Exception:
-                            file_info["game_state"] = "Could not retrieve schema"
-                            file_info["events"] = "Could not retrieve schema"
-                            file_info["features"] = "Could not retrieve schema"
-                        else:
-                            schema = json.loads(raw_schema)
-                            file_info["game_state"] = schema.get("game_state", "Could not find game state in schema")
-                            file_info["events"] = schema.get("events", "Could not find event listings in schema")
-                            file_info["features"] = schema.get("features", "Could not find feature configs in schema")
+                    file_info["detectors_link"] = f"{GITHUB_BASE_URL}{_matched_dataset.OGDRevision}/src/ogd/games/{sanitary_params.GameID.upper()}/detectors" if _matched_dataset.OGDRevision else None
+                    file_info["features_link"]  = f"{GITHUB_BASE_URL}{_matched_dataset.OGDRevision}/src/ogd/games/{sanitary_params.GameID.upper()}/features"  if _matched_dataset.OGDRevision else None
 
                     ret_val.RequestSucceeded(msg="Retrieved game file info by month", val=file_info)
                 else:
