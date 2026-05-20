b548043f4f97ccc0b84b2c1fe91d7f419cac90e1
Luke Swanson <lwswanson2@wisc.edu>
2026-05-20 17:44:34 -0500

    Bring GameList package module in line with convention.

--- package/src/ogd/apis/models/files/GameList.py
+++ package/src/ogd/apis/models/files/GameList.py
@@ -1,23 +1,52 @@
-from typing import Final, List
+import logging
+from dataclasses import dataclass
+from typing import Final, List, Optional
 
+from ogd.apis.models.APIRequest import APIRequest
 from ogd.apis.models.APIResponse import APIResponse
+from ogd.apis.models.enums.RESTType import RESTType
 from ogd.common.utils.typing import Map
 
-class GameList:
-    PATH : Final[str] = "/games"
+class GameListRequest(APIRequest):
+    def __init__(self, api_base_url:str, timeout:int=1):
+        _url = f"{api_base_url}/games"
+        super().__init__(url=_url, request_type=RESTType.GET, params=None, body=None, timeout=timeout)
 
-    def __init__(self, game_ids:List[str]):
-        self._game_ids = game_ids
+    def Execute(self, logger:Optional[logging.Logger]=None, retry:int=0) -> "GameList | APIResponse":
+        ret_val : GameList | APIResponse
+
+        api_response = super().Execute(logger=logger, retry=retry)
+        try:
+            ret_val = GameList.FromAPIResponse(response=api_response)
+        except (ValueError, KeyError):
+            ret_val = api_response
+
+        return ret_val
+
+@dataclass
+class GameList:
+    game_ids : List[str]
+
+    def __getitem__(self, index:int):
+        return self.GameIDs[index]
+
+    def __setitem__(self, index:int, value):
+        self.GameIDs[index] = value
 
     @property
     def GameIDs(self) -> List[str]:
-        return self._game_ids
+        return self.game_ids
+    
+    @staticmethod
+    def FromDict(raw_dict:Map) -> "GameList":
+        ret_val : GameList
 
-    @property
-    def AsDict(self) -> Map:
-        return {
-            "game_ids": self.GameIDs
-        }
+        if "game_ids" in raw_dict.keys():
+            ret_val = GameList(game_ids=raw_dict["game_ids"])
+        else:
+            raise KeyError(f"GameList source dict did not have game_ids element!")
+
+        return ret_val
     
     @staticmethod
     def FromAPIResponse(response:APIResponse) -> "GameList":
@@ -31,10 +60,7 @@ class GameList:
         ret_val : GameList
 
         if isinstance(response.Value, dict):
-            if all(key in response.Value.keys() for key in {"game_ids"}):
-                ret_val = GameList(
-                    game_ids=response.Value.get("game_ids", "GAMES NOT FOUND"),
-                )
-            else:
-                raise ValueError(f"APIResponse for GamesList had incorrect keys.")
+            ret_val = GameList.FromDict(response.Value)
+        else:
+            ret_val = GameList([])
         return ret_val
