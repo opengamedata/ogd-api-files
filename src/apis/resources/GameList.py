cb5f0c95b170182e4284190556b0ddf246709544
Luke Swanson <lwswanson2@wisc.edu>
2026-05-21 10:16:04 -0500

    Clean up logic in GameList resource.

--- src/apis/resources/GameList.py
+++ src/apis/resources/GameList.py
@@ -32,11 +32,11 @@ class GameList(Resource):
             file_list : DatasetRepositoryConfig = GetFileList(cfg.FileListURL)
 
             # If the given game isn't in our dictionary, or our dictionary doesn't have any date ranges for this game
-            if file_list.Games is None or len(file_list.Games) < 1:
-                ret_val.RequestErrored(msg="Could not find any games!", status=ResponseStatus.NOT_FOUND)
-            else:
+            if file_list.Games is not None and len(file_list.Games) > 0:
                 games = GameListModel(game_ids=list(file_list.Games.keys()))
                 ret_val.RequestSucceeded(msg="Retrieved list of games with available datasets", val=dataclasses.asdict(games))
+            else:
+                ret_val.RequestErrored(msg="Could not find any games!", status=ResponseStatus.NOT_FOUND)
         except Exception as err:
             msg = "Unexpected error while retrieving list of games with available datasets!"
             current_app.logger.error(f"{msg}\n{type(err)}:\n{err}")
