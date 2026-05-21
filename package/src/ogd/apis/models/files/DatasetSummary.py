bd387645cf724e15744d8fb8444cb7a36895079b
Luke Swanson <lwswanson2@wisc.edu>
2026-05-20 22:33:40 -0500

    Make DatasetSummary follow convention>' .

--- package/src/ogd/apis/models/files/DatasetSummary.py
+++ package/src/ogd/apis/models/files/DatasetSummary.py
@@ -9,134 +9,142 @@ from ogd.common.schemas.datasets.DatasetSchema import DatasetSchema
 from ogd.common.utils.typing import Map
 
 class DatasetSummaryRequest(APIRequest):
-    def __init__(self, api_base_url:str, game_id:str, timeout:int=1):
-        _url = f"{api_base_url}/games/{game_id}"
+    def __init__(self, api_base_url:str, game_id:str, year:int, month:int, timeout:int=1):
+        _url = f"{api_base_url}/games/{game_id}/datasets/{year}/{month}"
         super().__init__(url=_url, request_type=RESTType.GET, params=None, body=None, timeout=timeout)
 
-    def Execute(self, logger:Optional[logging.Logger]=None, retry:int=0) -> "GameSummary | APIResponse":
-        ret_val : GameSummary | APIResponse
+    def Execute(self, logger:Optional[logging.Logger]=None, retry:int=0) -> "DatasetSummary | APIResponse":
+        ret_val : DatasetSummary | APIResponse
 
         api_response = super().Execute(logger=logger, retry=retry)
         try:
-            ret_val = GameSummary.FromAPIResponse(response=api_response)
+            ret_val = DatasetSummary.FromAPIResponse(response=api_response)
         except (ValueError, KeyError):
             ret_val = api_response
 
         return ret_val
 
+@dataclass
 class DatasetSummary:
-    PATH : Final[str] = "/games/<string:game_id>/datasets/<int:year>"
-
-    def __init__(self, first_year:int, first_month:int, last_year:int,     last_month:int,
-                       raw_file:Optional[str],   events_file:Optional[str], sessions_file:Optional[str], players_file:Optional[str], population_file:Optional[str],
-                       events_template:Optional[str],  sessions_template:Optional[str],  players_template:Optional[str],  population_template:Optional[str],
-                       events_codespace:Optional[str], sessions_codespace:Optional[str], players_codespace:Optional[str], population_codespace:Optional[str],
-                       detectors_link:Optional[str],   features_link:Optional[str],      found_matching_range:bool):
-        self._first_year      = first_year
-        self._first_month     = first_month
-        self._last_year       = last_year
-        self._last_month      = last_month
-        self._raw_file        = raw_file
-        self._events_file     = events_file
-        self._sessions_file   = sessions_file
-        self._players_file    = players_file
-        self._population_file = population_file
-        self._events_template      = events_template
-        self._sessions_template    = sessions_template
-        self._players_template     = players_template
-        self._population_template  = population_template
-        self._events_codespace     = events_codespace
-        self._sessions_codespace   = sessions_codespace
-        self._players_codespace    = players_codespace
-        self._population_codespace = population_codespace
-        self._detectors_link       = detectors_link
-        self._features_link        = features_link
-        self._found_matching_range = found_matching_range
+    first_year           : int
+    first_month          : int
+    last_year            : int
+    last_month           : int
+    raw_file             : Optional[str]
+    events_file          : Optional[str]
+    sessions_file        : Optional[str]
+    players_file         : Optional[str]
+    population_file      : Optional[str]
+    events_template      : Optional[str]
+    sessions_template    : Optional[str]
+    players_template     : Optional[str]
+    population_template  : Optional[str]
+    events_codespace     : Optional[str]
+    sessions_codespace   : Optional[str]
+    players_codespace    : Optional[str]
+    population_codespace : Optional[str]
+    detectors_link       : Optional[str]
+    features_link        : Optional[str]
+    found_matching_range : bool
 
     @property
     def FirstYear(self) -> int:
-        return self._first_year
+        return self.first_year
     @property
     def FirstMonth(self) -> int:
-        return self._first_month
+        return self.first_month
     @property
     def LastYear(self) -> int:
-        return self._last_year
+        return self.last_year
     @property
     def LastMonth(self) -> int:
-        return self._last_month
+        return self.last_month
     @property
     def RawFile(self) -> Optional[str]:
-        return self._raw_file
+        return self.raw_file
     @property
     def EventsFile(self) -> Optional[str]:
-        return self._events_file
+        return self.events_file
     @property
     def SessionsFile(self) -> Optional[str]:
-        return self._sessions_file
+        return self.sessions_file
     @property
     def PlayersFile(self) -> Optional[str]:
-        return self._players_file
+        return self.players_file
     @property
     def PopulationFile(self) -> Optional[str]:
-        return self._population_file
+        return self.population_file
     @property
     def EventsTemplate(self) -> Optional[str]:
-        return self._events_template
+        return self.events_template
     @property
     def SessionsTemplate(self) -> Optional[str]:
-        return self._sessions_template
+        return self.sessions_template
     @property
     def PlayersTemplate(self) -> Optional[str]:
-        return self._players_template
+        return self.players_template
     @property
     def PopulationTemplate(self) -> Optional[str]:
-        return self._population_template
+        return self.population_template
     @property
     def EventsCodespace(self) -> Optional[str]:
-        return self._events_codespace
+        return self.events_codespace
     @property
     def SessionsCodespace(self) -> Optional[str]:
-        return self._sessions_codespace
+        return self.sessions_codespace
     @property
     def PlayersCodespace(self) -> Optional[str]:
-        return self._players_codespace
+        return self.players_codespace
     @property
     def PopulationCodespace(self) -> Optional[str]:
-        return self._population_codespace
+        return self.population_codespace
     @property
     def DetectorsLink(self) -> Optional[str]:
-        return self._detectors_link
+        return self.detectors_link
     @property
     def FeaturesLink(self) -> Optional[str]:
-        return self._features_link
+        return self.features_link
 
-    @property
-    def AsDict(self) -> Map:
-        return {
-            "first_year": self.FirstYear,
-            "first_month": self.FirstMonth,
-            "last_year": self.LastYear,
-            "last_month": self.LastMonth,
-            "raw_file": self.RawFile,
-            "events_file": self.EventsFile,
-            "sessions_file": self.SessionsFile,
-            "players_file": self.PlayersFile,
-            "population_file": self.PopulationFile,
-            "events_template": self.EventsTemplate,
-            "sessions_template": self.SessionsTemplate,
-            "players_template": self.PlayersTemplate,
-            "population_template": self.PopulationTemplate,
-            "events_codespace": self.EventsCodespace,
-            "sessions_codespace": self.SessionsCodespace,
-            "players_codespace": self.PlayersCodespace,
-            "population_codespace": self.PopulationCodespace,
-            "detectors_link": self.DetectorsLink,
-            "features_link": self.FeaturesLink,
-            "found_matching_range": self._found_matching_range
+    @staticmethod
+    def FromDict(raw_dict:Map) -> "DatasetSummary":
+        ret_val : DatasetSummary
+
+        expected_keys = {
+            "first_year", "first_month", "last_year", "last_month",
+            "raw_file", "events_file", "sessions_file", "players_file", "population_file",
+            "events_template", "sessions_template", "players_template", "population_template",
+            "events_codespace", "sessions_codespace", "players_codespace", "population_codespace",
+            "detectors_link", "features_link", "found_matching_range"
         }
+        missing_keys = expected_keys - raw_dict.keys()
+        if len(missing_keys) == 0:
+            ret_val = DatasetSummary(
+                first_year           = raw_dict.get("first_year", 0),
+                first_month          = raw_dict.get("first_month", 0),
+                last_year            = raw_dict.get("last_year", 0),
+                last_month           = raw_dict.get("last_month", 0),
+                raw_file             = raw_dict.get("raw_file"),
+                events_file          = raw_dict.get("events_file"),
+                sessions_file        = raw_dict.get("sessions_file"),
+                players_file         = raw_dict.get("players_file"),
+                population_file      = raw_dict.get("population_file"),
+                events_template      = raw_dict.get("events_template"),
+                sessions_template    = raw_dict.get("sessions_template"),
+                players_template     = raw_dict.get("players_template"),
+                population_template  = raw_dict.get("population_template"),
+                events_codespace     = raw_dict.get("events_codespace"),
+                sessions_codespace   = raw_dict.get("sessions_codespace"),
+                players_codespace    = raw_dict.get("players_codespace"),
+                population_codespace = raw_dict.get("population_codespace"),
+                detectors_link       = raw_dict.get("detectors_link"),
+                features_link        = raw_dict.get("features_link"),
+                found_matching_range = raw_dict.get("found_matching_range", False)
+            )
+        else:
+            raise KeyError(f"DatasetSummary source dict had incorrect set of keys, missing {missing_keys}")
 
-            
+        return ret_val
+
     
     @staticmethod
     def FromAPIResponse(response:APIResponse) -> "DatasetSummary":
@@ -149,37 +157,9 @@ class DatasetSummary:
         """
         ret_val : DatasetSummary
 
-        if isinstance(response.Value, dict):
-            expected_keys = {
-                "first_year", "first_month", "last_year", "last_month",
-                "raw_file", "events_file", "sessions_file", "players_file", "population_file",
-                "events_template", "sessions_template", "players_template", "population_template",
-                "events_codespace", "sessions_codespace", "players_codespace", "population_codespace",
-                "detectors_link", "features_link", "found_matching_range"
-            }
-            if all(key in response.Value.keys() for key in expected_keys):
-                ret_val = DatasetSummary(
-                    first_year=response.Value.get("first_year", 0),
-                    first_month=response.Value.get("first_month", 0),
-                    last_year=response.Value.get("last_year", 0),
-                    last_month=response.Value.get("last_month", 0),
-                    raw_file=response.Value.get("raw_file"),
-                    events_file=response.Value.get("events_file"),
-                    sessions_file=response.Value.get("sessions_file"),
-                    players_file=response.Value.get("players_file"),
-                    population_file=response.Value.get("population_file"),
-                    events_template=response.Value.get("events_template"),
-                    sessions_template=response.Value.get("sessions_template"),
-                    players_template=response.Value.get("players_template"),
-                    population_template=response.Value.get("population_template"),
-                    events_codespace=response.Value.get("events_codespace"),
-                    sessions_codespace=response.Value.get("sessions_codespace"),
-                    players_codespace=response.Value.get("players_codespace"),
-                    population_codespace=response.Value.get("population_codespace"),
-                    detectors_link=response.Value.get("detectors_link"),
-                    features_link=response.Value.get("features_link"),
-                    found_matching_range=response.Value.get("found_matching_range", False)
-                )
-            else:
-                raise ValueError(f"APIResponse for DatasetInfo had incorrect keys.")
+        if response.Value is not None:
+            ret_val = DatasetSummary.FromDict(raw_dict=response.Value)
+        else:
+            raise ValueError(f"Response for DataSummary returned no values!")
+
         return ret_val
