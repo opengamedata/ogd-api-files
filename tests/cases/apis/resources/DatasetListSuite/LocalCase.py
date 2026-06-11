d65d2d03a4fb8300e9513258a4c9f441a3932920
Luke Swanson <lwswanson2@wisc.edu>
2026-06-11 18:31:35 -0500

    Work on the local case for DatasetList.

--- tests/cases/apis/resources/DatasetListSuite/LocalCase.py
+++ tests/cases/apis/resources/DatasetListSuite/LocalCase.py
@@ -1,16 +1,14 @@
 # import libraries
 import logging
-import unittest
 from json.decoder import JSONDecodeError
 from typing import Optional
 from unittest import TestCase
 # import 3rd-party libraries
-import requests
 from flask import Flask
 from werkzeug.test import TestResponse
 # import ogd libraries
-from ogd.apis.models.APIRequest import APIRequest
 from ogd.apis.models.APIResponse import APIResponse, ResponseStatus
+from ogd.apis.models.enums.RESTType import RESTType
 from ogd.common.utils.Logger import Logger
 # import locals
 from src.configs.FileAPIConfig import FileAPIConfig
@@ -46,45 +44,38 @@ class LocalCase(TestCase):
 
         cls.server = cls.application.test_client()
 
-        cls.url    = f"/games/AQUALAB/datasets"
-        Logger.Log(f"Sending request to {cls.url}", logging.INFO)
-        cls.result = cls.server.get(cls.url)
-        if cls.result is not None:
-            try:
-                _raw = cls.result.json
-            except JSONDecodeError as err:
-                print(f"Could not parse {cls.result.text} to JSON!\n{err}")
-            else:
-                cls.content = APIResponse.FromDict(all_elements=_raw or {}, status=ResponseStatus(cls.result.status_code))
-
-    def test_Responded(self):
-        self.assertIsNotNone(self.content, f"No result from request to {self.url}")
-
-    def test_Succeeded(self):
-        if self.content is not None:
-            self.assertEqual(self.content.Status, ResponseStatus.OK)
+    def test_get(self):
+        _url    = f"/games/AQUALAB/datasets"
+        # 1. Run request
+        raw_response = self.server.get(_url)
+        try:
+            response = APIResponse.FromDict(all_elements=raw_response.json or {}, status=ResponseStatus(raw_response.status_code))
+        except JSONDecodeError as err:
+            self.fail(f"Could not parse {raw_response.text} to JSON!\n{err}")
+        raw_response.close()
+        # 2. Perform assertions
+        if response:
+            self.assertIsNotNone(response, f"No result from request to {_url}")
+            self.assertTrue(response.OK, f"Bad status from {_url}")
+            self.assertEqual(response.Type, RESTType.GET, f"Bad type from {_url}")
+            self.assertIsInstance(response.Value, dict, f"Bad value type from {_url}")
+            if response.Value:
+                # check game ID
+                self.assertIn("game_id", response.Value.keys(), "Response did not contain game_id")
+                self.assertEqual(response.Value.get("game_id", "NOT FOUND"), "AQUALAB")
+                # check sessions element
+                self.assertIn("datasets", response.Value.keys(), "Response did not contain datasets")
+                datasets = response.Value.get('datasets', [])
+                self.assertIsInstance(datasets, list)
+                self.assertGreaterEqual(len(datasets), 17) # Aqualab should definitely have more than 17 months, as long as it's been around.
+                expected_data = {
+                    'year': 2022,
+                    'month': 8,
+                    'total_sessions': 357,
+                    "sessions_file"   : "https://opengamedata.fielddaylab.wisc.edu/data/AQUALAB/AQUALAB_20220801_to_20220831_0c348a5_session-features.zip",
+                    "players_file"    : "https://opengamedata.fielddaylab.wisc.edu/data/AQUALAB/AQUALAB_20220801_to_20220831_0c348a5_player-features.zip",
+                    "population_file" : "https://opengamedata.fielddaylab.wisc.edu/data/AQUALAB/AQUALAB_20220801_to_20220831_0c348a5_population-features.zip"
+                }
+                self.assertEqual(datasets[17], expected_data)
         else:
-            self.fail(f"No result from request to {self.url}")
-
-    def test_Correct(self):
-        _expected_data = {
-            'year': 2022,
-            'month': 8,
-            'total_sessions': 357,
-            "sessions_file"   : "https://opengamedata.fielddaylab.wisc.edu/data/AQUALAB/AQUALAB_20220801_to_20220831_0c348a5_session-features.zip",
-            "players_file"    : "https://opengamedata.fielddaylab.wisc.edu/data/AQUALAB/AQUALAB_20220801_to_20220831_0c348a5_player-features.zip",
-            "population_file" : "https://opengamedata.fielddaylab.wisc.edu/data/AQUALAB/AQUALAB_20220801_to_20220831_0c348a5_population-features.zip"
-        }
-        if self.content is not None and self.content.Value is not None:
-            self.assertIsInstance(self.content.Value, dict)
-            # check game ID
-            self.assertIn("game_id", self.content.Value.keys(), "Response did not contain game_id")
-            self.assertEqual(self.content.Value.get("game_id", "NOT FOUND"), "AQUALAB")
-            # check sessions element
-            self.assertIn("datasets", self.content.Value.keys(), "Response did not contain datasets")
-            datasets = self.content.Value.get('datasets', [])
-            self.assertIsInstance(datasets, list)
-            self.assertGreaterEqual(len(datasets), 17) # Aqualab should definitely have more than 17 months, as long as it's been around.
-            self.assertEqual(datasets[17], _expected_data)
-        else:
-            self.fail(f"No JSON content from request to {self.url}")
+            self.fail("Could not generate APIResponse from test response")
