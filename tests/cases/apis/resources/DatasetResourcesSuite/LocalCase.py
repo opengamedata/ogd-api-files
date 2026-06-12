a7d2abf0f0c44923a2546a48b535ee8208112824
Luke Swanson <lwswanson2@wisc.edu>
2026-06-11 20:20:10 -0500

    Re-implement dataset resources cases.

--- tests/cases/apis/resources/DatasetResourcesSuite/LocalCase.py
+++ tests/cases/apis/resources/DatasetResourcesSuite/LocalCase.py
@@ -1,16 +1,12 @@
 # import libraries
 import logging
-import unittest
 from json.decoder import JSONDecodeError
-from typing import Optional
 from unittest import TestCase
 # import 3rd-party libraries
-import requests
 from flask import Flask
-from werkzeug.test import TestResponse
 # import ogd libraries
-from ogd.apis.models.APIRequest import APIRequest
 from ogd.apis.models.APIResponse import APIResponse, ResponseStatus
+from ogd.apis.models.enums.RESTType import RESTType
 from ogd.common.utils.Logger import Logger
 # import locals
 from src.configs.FileAPIConfig import FileAPIConfig
@@ -18,22 +14,20 @@ from src.apis.FileAPI import FileAPI
 from tests.FileAPITestConfig import FileAPITestConfig
 from tests.config.t_config import settings
 
-class test_GameDatasetInfo_local(TestCase):
+class LocalCase(TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.url    : str
-        cls.result : Optional[TestResponse]
-        cls.content : Optional[APIResponse]    = None
-
         # 1. Get testing config
-        cls.testing_cfg = FileAPITestConfig.FromDict(name="FileAPITestConfig", unparsed_elements=settings)
-        _level     = logging.DEBUG if cls.testing_cfg.Verbose else logging.INFO
-        _str_level =       "DEBUG" if cls.testing_cfg.Verbose else "INFO"
-        Logger.std_logger.setLevel(_level)
+        testing_cfg = FileAPITestConfig.FromDict(name="FileAPITestConfig", unparsed_elements=settings)
+
+        _level     = logging.DEBUG if testing_cfg.Verbose else logging.INFO
+        _str_level =       "DEBUG" if testing_cfg.Verbose else "INFO"
+        Logger.InitializeLogger(level=_level, use_logfile=False)
 
         # 2. Set up local Flask app to run tests
         cls.application = Flask(__name__)
         cls.application.logger.setLevel(_level)
+        cls.application.secret_key = b'thisisafakesecretkey'
 
         _server_cfg_elems = {
             "API_VERSION"   : "0.0.0-Testing",
@@ -46,59 +40,45 @@ class test_GameDatasetInfo_local(TestCase):
 
         cls.server = cls.application.test_client()
 
-        cls.url    = f"/games/AQUALAB/datasets/2024/1"
-        Logger.Log(f"Sending request to {cls.url}", logging.INFO)
-        cls.result = cls.server.get(cls.url)
-        if cls.result is not None:
-            try:
-                _raw = cls.result.json
-            except JSONDecodeError as err:
-                print(f"Could not parse {cls.result.text} to JSON!\n{err}")
-            else:
-                cls.content = APIResponse.FromDict(all_elements=_raw or {}, status=ResponseStatus(cls.result.status_code))
-
-    def test_Responded(self):
-        self.assertIsNotNone(self.content, f"No result from request to {self.url}")
-
-    def test_Succeeded(self):
-        if self.content is not None:
-            self.assertEqual(self.content.Status, ResponseStatus.OK)
+    def test_get(self):
+        _url = "/games/AQUALAB/datasets/2024/1"
+        # 1. Run request
+        raw_response = self.server.get(_url)
+        try:
+            response = APIResponse.FromDict(all_elements=raw_response.json or {}, status=ResponseStatus(raw_response.status_code))
+        except JSONDecodeError as err:
+            self.fail(f"Could not parse {raw_response.text} to JSON!\n{err}")
+        raw_response.close()
+        # 2. Perform assertions
+        if response:
+            self.assertIsNotNone(response, f"No response from {_url}")
+            self.assertTrue(response.OK, f"Bad status from {_url}")
+            self.assertEqual(response.Type, RESTType.GET, f"Bad type from {_url}")
+            self.assertIsInstance(response.Value, dict, f"Bad value type from {_url}")
+            if response.Value:
+                expected_data = {
+                    "start_date":"01/01/2024",
+                    "end_date":"01/31/2024",
+                    "detectors_link":"https://github.com/opengamedata/opengamedata-core/tree/df72162/src/ogd/games/AQUALAB/detectors",
+                    "events_codespace":"https://codespaces.new/opengamedata/opengamedata-samples/tree/aqualab?quickstart=1&devcontainer_path=.devcontainer%2Fevent-template%2Fdevcontainer.json",
+                    "all_events_file":"https://opengamedata.fielddaylab.wisc.edu/data/AQUALAB/AQUALAB_20240101_to_20240131_df72162_all-events.zip",
+                    "events_template":"https://github.com/opengamedata/opengamedata-templates/tree/aqualab",
+                    "features_link":"https://github.com/opengamedata/opengamedata-core/tree/df72162/src/ogd/games/AQUALAB/features",
+                    "combined_features_file":None,
+                    "players_codespace":"https://codespaces.new/opengamedata/opengamedata-samples/tree/aqualab?quickstart=1&devcontainer_path=.devcontainer%player-template%2Fdevcontainer.json",
+                    "players_file":"https://opengamedata.fielddaylab.wisc.edu/data/AQUALAB/AQUALAB_20240101_to_20240131_df72162_player-features.zip",
+                    "players_template":"https://github.com/opengamedata/opengamedata-templates/tree/aqualab",
+                    "population_file":"https://opengamedata.fielddaylab.wisc.edu/data/AQUALAB/AQUALAB_20240101_to_20240131_df72162_population-features.zip",
+                    "population_template":"https://github.com/opengamedata/opengamedata-templates/tree/aqualab",
+                    "population_codespace":"https://codespaces.new/opengamedata/opengamedata-samples/tree/aqualab?quickstart=1&devcontainer_path=.devcontainer%population-template%2Fdevcontainer.json",
+                    "game_events_file":"https://opengamedata.fielddaylab.wisc.edu/data/AQUALAB/AQUALAB_20240101_to_20240131_df72162_events.zip",
+                    "sessions_codespace":	"https://codespaces.new/opengamedata/opengamedata-samples/tree/aqualab?quickstart=1&devcontainer_path=.devcontainer%session-template%2Fdevcontainer.json",
+                    "sessions_file":"https://opengamedata.fielddaylab.wisc.edu/data/AQUALAB/AQUALAB_20240101_to_20240131_df72162_session-features.zip",
+                    "sessions_template":"https://github.com/opengamedata/opengamedata-templates/tree/aqualab",
+                }
+                self.assertEqual(set(response.Value.keys()), set(expected_data.keys()), msg="Mismatching keys between response and expected")
+                for key, val in expected_data.items():
+                    with self.subTest(key=key, val=val):
+                        self.assertEqual(response.Value.get(key), val, msg=f"Mismatching value for key {key}")
         else:
-            self.fail(f"No result from request to {self.url}")
-
-    @unittest.skip(reason="Temporarily turning this off until we solve issue with changing hashs for detectors/features links.")
-    def test_Correct(self):
-        expected_data = {
-            "detectors_link":"https://github.com/opengamedata/opengamedata-core/tree/42597ba/src/ogd/games/AQUALAB/detectors",
-            "events_codespace":"https://codespaces.new/opengamedata/opengamedata-samples/tree/aqualab?quickstart=1&devcontainer_path=.devcontainer%2Fevent-template%2Fdevcontainer.json",
-            "events_file":"https://opengamedata.fielddaylab.wisc.edu/data/AQUALAB/AQUALAB_20240101_to_20240131_df72162_all-events.zip",
-            "events_template":"https://github.com/opengamedata/opengamedata-templates/tree/aqualab",
-            "features_link":"https://github.com/opengamedata/opengamedata-core/tree/42597ba/src/ogd/games/AQUALAB/features",
-            "first_month":1,
-            "first_year":2024,
-            "found_matching_range":True,
-            "last_month":1,
-            "last_year":2024,
-            "players_codespace":"https://codespaces.new/opengamedata/opengamedata-samples/tree/aqualab?quickstart=1&devcontainer_path=.devcontainer%2Fsession-template%2Fdevcontainer.json",
-            "players_file":"https://opengamedata.fielddaylab.wisc.edu/data/AQUALAB/AQUALAB_20240101_to_20240131_df72162_player-features.zip",
-            "players_template":"https://github.com/opengamedata/opengamedata-templates/tree/aqualab",
-            "population_file":"https://opengamedata.fielddaylab.wisc.edu/data/AQUALAB/AQUALAB_20240101_to_20240131_df72162_population-features.zip",
-            "population_template":"https://github.com/opengamedata/opengamedata-templates/tree/aqualab",
-            "population_codespace":"https://codespaces.new/opengamedata/opengamedata-samples/tree/aqualab?quickstart=1&devcontainer_path=.devcontainer%2Fpopulation-template%2Fdevcontainer.json",
-            "raw_file":"https://opengamedata.fielddaylab.wisc.edu/data/AQUALAB/AQUALAB_20240101_to_20240131_df72162_events.zip",
-            "sessions_codespace":"https://codespaces.new/opengamedata/opengamedata-samples/tree/aqualab?quickstart=1&devcontainer_path=.devcontainer%2Fplayer-template%2Fdevcontainer.json",
-            "sessions_file":"https://opengamedata.fielddaylab.wisc.edu/data/AQUALAB/AQUALAB_20240101_to_20240131_df72162_session-features.zip",
-            "sessions_template":"https://github.com/opengamedata/opengamedata-templates/tree/aqualab",
-        }
-        # HACK: Currently, there's some mismatches in naming conventions between old and new, as well as in how we're parsing out what's what.
-        # So, temporarily changing the "expected" from what would be truly ideal behavior, to what is expected given the shortcomings of the packages we're depending on.
-        # This allows us to watch for regressions due to changes in the FileAPI code, and can be reverted when we get upstream improvements to the packages/what's actually named what on the server
-        expected_data['raw_file'] = expected_data['events_file']
-        expected_data['events_file'] = None
-
-        if self.content is not None and self.content.Value is not None:
-            self.assertEqual(set(self.content.Value.keys()), set(expected_data.keys()), msg="Mismatching keys between response and expected")
-            for key, val in expected_data.items():
-                self.assertEqual(self.content.Value.get(key), val, msg=f"Mismatch for key {key}")
-        else:
-            self.fail(f"No JSON content from request to {self.url}")
+            self.fail("Could not generate APIResponse from test response")
