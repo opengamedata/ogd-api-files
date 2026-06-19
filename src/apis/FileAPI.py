7a5774cd6c43086637f5ac5defe8eb839e1db5f6
Luke Swanson <lwswanson2@wisc.edu>
2026-06-19 16:40:44 -0500

    Ok, need to have imports next to use, per-resource, similar to what we do per-API at the app level.

--- src/apis/FileAPI.py
+++ src/apis/FileAPI.py
@@ -3,13 +3,6 @@ from flask import Flask
 from flask_restful import Api
 
 # import local files
-from apis.resources.GameList import GameList
-from apis.resources.GameSummaries import GameSummaries
-from apis.resources.GameSummary import GameSummary
-from apis.resources.DatasetList import DatasetList
-from apis.resources.DatasetResources import DatasetResources
-from apis.resources.DatasetFile import DatasetFile
-from apis.resources.DatasetManifest import DatasetManifest
 from configs.FileAPIConfig import FileAPIConfig
 
 class FileAPI:
@@ -31,32 +24,39 @@ class FileAPI:
         api = Api(app)
 
         try:
+            from apis.resources.GameList import GameList
             api.add_resource(GameList,         '/games')
         except Exception as err:
-            app.logger.warning(f"Couldn't register GameList resource:\n{err}")
+            app.logger.warning(f"Couldn't register GameList resource:\n   {err}")
         try:
+            from apis.resources.GameSummaries import GameSummaries
             api.add_resource(GameSummaries,    '/games/details')
         except Exception as err:
-            app.logger.warning(f"Couldn't register GameSummaries resource:\n{err}")
+            app.logger.warning(f"Couldn't register GameSummaries resource:\n   {err}")
         try:
+            from apis.resources.GameSummary import GameSummary
             api.add_resource(GameSummary,      '/games/<string:game_id>')
         except Exception as err:
-            app.logger.warning(f"Couldn't register GameSummary resource:\n{err}")
+            app.logger.warning(f"Couldn't register GameSummary resource:\n   {err}")
         try:
+            from apis.resources.DatasetList import DatasetList
             api.add_resource(DatasetList,      '/games/<string:game_id>/datasets',
                                                '/games/<string:game_id>/datasets/<int:year>')
         except Exception as err:
-            app.logger.warning(f"Couldn't register DatasetList resource:\n{err}")
+            app.logger.warning(f"Couldn't register DatasetList resource:\n   {err}")
         try:
+            from apis.resources.DatasetResources import DatasetResources
             api.add_resource(DatasetResources, '/games/<string:game_id>/datasets/<int:year>/<int:month>')
         except Exception as err:
-            app.logger.warning(f"Couldn't register DatasetResources resource:\n{err}")
+            app.logger.warning(f"Couldn't register DatasetResources resource:\n   {err}")
         try:
+            from apis.resources.DatasetFile import DatasetFile
             api.add_resource(DatasetManifest,  '/games/<string:game_id>/datasets/<int:year>/<int:month>/manifest')
         except Exception as err:
-            app.logger.warning(f"Couldn't register DatasetManifest resource:\n{err}")
+            app.logger.warning(f"Couldn't register DatasetManifest resource:\n   {err}")
         try:
+            from apis.resources.DatasetManifest import DatasetManifest
             api.add_resource(DatasetFile,      '/games/<string:game_id>/datasets/<int:year>/<int:month>/<string:file_type>')
         except Exception as err:
-            app.logger.warning(f"Couldn't register DatasetFile resource:\n{err}")
+            app.logger.warning(f"Couldn't register DatasetFile resource:\n   {err}")
         FileAPI.server_config = settings
